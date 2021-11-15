bacce3eb1e8251aec140f9943fea991fc1d3f833
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:58:11 -0300

    Categoria class

---
+++ Categoria.java
@@ -0,0 +1,16 @@
+public class Categoria {
+    private final String nombre;
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
