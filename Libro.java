9214edd53490860c20854a1bdd5e28527f0fe971
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 20:02:42 -0300

    Libro y Revista class

---
+++ Libro.java
@@ -0,0 +1,6 @@
+public class Libro extends Publicacion {
+
+    public Libro(String titulo, Autor autor, Editorial editorial, Categoria categoria) {
+        super(titulo, autor, editorial, categoria);
+    }
+}
---
+++ Revista.java
@@ -0,0 +1,6 @@
+public class Revista extends Publicacion {
+
+    public Revista(String titulo, Autor autor, Editorial editorial, Categoria categoria) {
+        super(titulo, autor, editorial, categoria);
+    }
+}
