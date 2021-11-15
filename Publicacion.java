24f63316a47cb1ef25a9d49dd42266ebce1a7449
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 20:01:46 -0300

    Publicacion class

---
+++ Publicacion.java
@@ -0,0 +1,40 @@
+public abstract class Publicacion {
+    private final String titulo;
+    private final Autor autor;
+    private final Editorial editorial;
+    private final Categoria categoria;
+    private int cantidadPrestamos;
+
+    public Publicacion(String titulo, Autor autor, Editorial editorial, Categoria categoria) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public int getCantidadPrestamos() {
+        return cantidadPrestamos;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + autor + editorial + categoria + cantidadPrestamos;
+    }
+}
+
