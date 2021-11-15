d73021e4e0350b6a72a75113fe7110df7ea0e08b
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:21:10 -0300

    Localidad class

---
+++ Localidad.java
@@ -0,0 +1,22 @@
+public class Localidad {
+    private final String nombre;
+    private final Provincia provincia;
+
+    public Localidad(String nombre, Provincia provincia) {
+        this.nombre = nombre;
+        this.provincia = provincia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Provincia getProvincia() {
+        return provincia;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + provincia;
+    }
+}
