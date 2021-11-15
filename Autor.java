b1f2ce7a9603ec61c75a0e2e6242782d21451c05
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:16:30 -0300

    Autor class

---
+++ Autor.java
@@ -0,0 +1,16 @@
+public class Autor {
+    private final String nombre;
+
+    public Autor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
