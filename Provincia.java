a5c59129158d2d0db3e61e4d229603a4bbd604d8
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:18:08 -0300

    Provincia class

---
+++ Provincia.java
@@ -0,0 +1,16 @@
+public class Provincia {
+    private final String nombre;
+
+    public Provincia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
