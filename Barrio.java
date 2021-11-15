2fe1c3227ee24a3e3d1b9a619e73511c30f4e2fc
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:22:39 -0300

    Barrio class

---
+++ Barrio.java
@@ -0,0 +1,22 @@
+public class Barrio {
+    private final String nombre;
+    private final Localidad localidad;
+
+    public Barrio(String nombre, Localidad localidad) {
+        this.nombre = nombre;
+        this.localidad = localidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Localidad getLocalidad() {
+        return localidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + localidad;
+    }
+}
