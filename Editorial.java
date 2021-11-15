291967ae15095e570fd891d55f48a2462b627985
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 19:57:07 -0300

    Editorial class

---
+++ Editorial.java
@@ -0,0 +1,16 @@
+public class Editorial {
+    private final String nombre;
+
+    public Editorial(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
